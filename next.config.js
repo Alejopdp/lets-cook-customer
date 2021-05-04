59ca0db9cc7d089b7109b99e6184075e16b55881
Alejopdp <alejoscotti@gmail.com>
2021-05-04 11:41:17 +0200

    Inicializo repo

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// next.config.js
+module.exports = {
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["en", "es"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "es",
+  },
+  // IT'S DEPRECATED
+  // publicRuntimeConfig: {
+  //   // Will be available on both server and client
+  //   staticFolder: '/static',
+  // },
+};
---
+++ theme.js
@@ -0,0 +1,69 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import colors from "./styles/colors.module.scss";
+import fonts from "./styles/fonts.module.scss";
+
+// Create a theme instance.
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: colors.primaryColor,
+            contrastText: colors.contrastColor,
+        },
+        secondary: {
+            main: colors.secondaryColor,
+        },
+        background: {
+            default: colors.backgroundSecondaryColor,
+            paper: colors.backgroundPrimaryColor,
+        },
+        text: {
+            primary: colors.titleColor,
+            secondary: colors.paragraphColor,
+            danger: colors.dangerColor
+        },
+    },
+
+  typography: {
+    h1: { fontFamily: fonts.titleFont, fontWeight: 800 },
+    h2: { fontFamily: fonts.titleFont, fontWeight: 700 },
+    h3: { fontFamily: fonts.titleFont, fontWeight: 700 },
+    h4: { fontFamily: fonts.titleFont, fontWeight: 700 },
+    h5: { fontFamily: fonts.titleFont, fontWeight: 700 },
+    h6: { fontFamily: fonts.titleFont, fontWeight: 700 },
+    subtitle1: { fontFamily: fonts.titleFont, fontWeight: 700 },
+    subtitle2: { fontFamily: fonts.titleFont, fontWeight: 700 },
+    body1: { fontFamily: fonts.paragraphFont, fontWeight: 400 },
+    body2: { fontFamily: fonts.paragraphFont, fontWeight: 400 },
+    button: { fontFamily: fonts.titleFont, fontWeight: 800 },
+  },
+
+  overrides: {
+    MuiButton: {
+      // Name of the component ⚛️ / style sheet
+      contained: {
+        backgroundColor: colors.backgroundSecondaryColor,
+      },
+    },
+    MuiInput: {
+      input: {
+        root: {
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+        },
+        "&::placeholder": {
+          textOverflow: "ellipsis",
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+        },
+      },
+      underline: {
+        "&&&&:before": {
+          border: 0,
+        },
+      },
+    },
+  },
+});
+
+export default theme;
