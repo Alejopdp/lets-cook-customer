b26f1c6ac5d05198ad84759d13d8afb58ed3440b
Irungaray <LionelIrungaray@GMail.com>
2021-05-13 18:06:25 -0300

    Idiomas

--- components/atoms/LegalTextSection/LegalTextSection.jsx
+++ components/atoms/LegalTextSection/LegalTextSection.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
+import { useRouter } from "next/router";
+const langs = require("../../../lang").LegalTextSection;
 
 // External components
 import Grid from "@material-ui/core/Grid";
@@ -19,87 +21,56 @@ const useStyles = makeStyles((theme) => ({
 
 const LegalTextSection = () => {
     const classes = useStyles();
-    <Typography paragraph variant="body1"></Typography>;
+    const router = useRouter();
+    const lang = langs[router.locale];
 
     return (
         <Grid container direction="column" className={clsx(classes.padd6, classes.align)}>
             <Grid item>
-                <Typography variant="subtitle1">I. Información general</Typography>
+                {/* Párrafo 1 */}
+                <Typography variant="subtitle1">{lang.firstParagraph.subtitle}</Typography>
                 <Typography paragraph variant="body1">
-                    En cumplimiento con el deber de información dispuesto en la Ley 34/2002 de Servicios de la Sociedad de la INformación y
-                    el Comercio Electrónico (LSSI-CE) de 11 de julio, se facilitan a continuación los siguientes datos de información
-                    general de este sitio web:
+                    {lang.firstParagraph.paragraph1}
                 </Typography>
                 <Typography paragraph variant="body1">
-                    La titularidad de este sitio web, www.letscooknow.es, (en adelante, Sitio Web) la ostenta: Let's Cook S.L., con CIF:
-                    B67476051, y cuyos datos de contacto son:
+                    {lang.firstParagraph.paragraph2}
                 </Typography>
                 <Typography paragraph variant="body1">
-                    Dirección: <br />
+                    {lang.firstParagraph.adress} <br />
                     Passeig de Sant Joan, 93 <br />
                     08009 Barcelona <br />
-                    Email de contacto: info@letscooknow.es <br />
+                    {lang.firstParagraph.email} info@letscooknow.es <br />
                 </Typography>
 
-                <Typography variant="subtitle1">II. TÉRMINOS Y CONDICIONES GENERALES DE USO</Typography>
+                {/* Párrafo 2 */}
+                <Typography variant="subtitle1">{lang.secondParagraph.subtitle}</Typography>
                 <Typography paragraph variant="body1">
-                    El objeto de las condiciones: El Sitio Web
+                    {lang.secondParagraph.paragraph1}
                 </Typography>
                 <Typography paragraph variant="body1">
-                    El objeto de las presentes Condiciones Generales de Uso (en adelante, Condiciones) es regular el acceso y la utilización
-                    del Sitio Web. A los efectos de las presentes Condiciones se entenderá como Sitio Web: la apariencia externa de los
-                    interfaces de pantalla, tanto de forma estática como de forma dinámica, es decir, el árbol de navegación; y todos los
-                    elementos integrados tanto en los interfaces de pantalla como en el árbol de navegación (en adelante, Contenidos) y
-                    todos aquellos servicios o recursos en línea que en su caso ofrezca a los Usuarios (en adelante, Servicios).
+                    {lang.secondParagraph.paragraph2}
                 </Typography>
                 <Typography paragraph variant="body1">
-                    Let's cook se reserva la facultad de modificar, en cualquier momento, y sin aviso previo, la presentación y
-                    configuración del Sitio Web y de los Contenidos y Servicios que en él pudieran estar incorporados. El Usuario reconoce y
-                    acepta que en cualquier momento Let's cook pueda interrumpir, desactivar y/o cancelar cualquiera de estos elementos que
-                    se integran en el Sitio Web o el acceso a los mismos.
+                    {lang.secondParagraph.paragraph3}
                 </Typography>
                 <Typography paragraph variant="body1">
-                    El acceso al Sitio Web por el Usuario tiene carácter libre y, por regla general, es gratuito sin que el Usuario tenga
-                    que proporcionar una contraprestación para poder disfrutar de ello, salvo en lo relativo al coste de conexión a través
-                    de la red de telecomunicaciones suministrada por el proveedor de acceso que hubiere contratado el Usuario.
+                    {lang.secondParagraph.paragraph4}
                 </Typography>
                 <Typography paragraph variant="body1">
-                    La utilización de alguno de los Contenidos o Servicios del Sitio Web podrá hacerse mediante la suscripción o registro
-                    previo del Usuario.
+                    {lang.secondParagraph.paragraph5}
                 </Typography>
 
-                <Typography variant="subtitle1">El Usuario</Typography>
+                {/* Párrafo 3 */}
+                <Typography variant="subtitle1">{lang.thirdParagraph.subtitle}</Typography>
                 <Typography paragraph variant="body1">
-                    El acceso, la navegación y uso del Sitio Web, así como por los espacios habilitados para interactuar entre los Usuarios,
-                    y el Usuario y Let's cook, como los comentarios y/o espacios de blogging, confiere la condición de Usuario, por lo que
-                    se aceptan, desde que se inicia la navegación por el Sitio Web, todas las Condiciones aquí establecidas, así como sus
-                    ulteriores modificaciones, sin perjuicio de la aplicación de la correspondiente normativa legal de obligado cumplimiento
-                    según el caso. Dada la relevancia de lo anterior, se recomienda al Usuario leerlas cada vez que visite el Sitio Web.
+                    {lang.thirdParagraph.paragraph1}
                 </Typography>
                 <Typography paragraph variant="body1">
-                    El Sitio Web de Let's cook proporciona gran diversidad de información, servicios y datos. El Usuario asume su
-                    responsabilidad para realizar un uso correcto del Sitio Web. Esta responsabilidad se extenderá a:
+                    {lang.thirdParagraph.paragraph2}
                 </Typography>
                 <Typography paragraph variant="body1">
-                    Un uso de la información, Contenidos y/o Servicios y datos ofrecidos por Let's cook sin que sea contrario a lo dispuesto
-                    por las presentes Condiciones, la Ley, la moral o el orden público, o que de cualquier otro modo puedan suponer lesión
-                    de los derechos de terceros o del mismo funcionamiento del Sitio Web. La veracidad y licitud de las informaciones
-                    aportadas por el Usuario en los formularios extendidos por Let's cook para el acceso a ciertos Contenidos o Servicios
-                    ofrecidos por el Sitio Web. En todo caso, el Usuario notificará de forma inmediata a Let's cook acerca de cualquier
-                    hecho que permita el uso indebido de la información registrada en dichos formularios, tales como, pero no sólo, el robo,
-                    extravío, o el acceso no autorizado a identificadores y/o contraseñas, con el fin de proceder a su inmediata
-                    cancelación. Let's cook se reserva el derecho de retirar todos aquellos comentarios y aportaciones que vulneren la ley,
-                    el respeto a la dignidad de la persona, que sean discriminatorios, xenófobos, racistas, pornográficos, spamming, que
-                    atenten contra la juventud o la infancia, el orden o la seguridad pública o que, a su juicio, no resultaran adecuados
-                    para su publicación.
+                    {lang.thirdParagraph.paragraph3}
                 </Typography>
-                <Typography paragraph variant="body1"></Typography>
-                <Typography paragraph variant="body1"></Typography>
-                <Typography paragraph variant="body1"></Typography>
-                <Typography paragraph variant="body1"></Typography>
-                <Typography paragraph variant="body1"></Typography>
-                <Typography paragraph variant="body1"></Typography>
-                <Typography paragraph variant="body1"></Typography>
             </Grid>
         </Grid>
     );
---
+++ lang/components/atoms/LegalTextSection.jsx
@@ -0,0 +1,34 @@
+export const LegalTextSection = {
+  es: {
+    firstParagraph: {
+      subtitle: "I. INFORMACIÓN GENERAL",
+      paragraph1: "En cumplimiento con el deber de información dispuesto en la Ley 34/2002 de Servicios de la SOciedad de la Información y el Comercio Electrónico (LSSI-CE) de 11 de julio, se facilitan a continuación los siguientes datos de información general de este sitio web:",
+      paragraph2: "La titularidad de este sitio web, www.letscooknow.es, (en adelante, Sitio Web) la ostenta: Let's Cook S.L., con CIF: B67476051, y cuyos datos de contacto son: ",
+      adress: "Dirección:",
+      email: "Email de contacto:",
+    },
+    secondParagraph: {
+      subtitle: "II. TÉRMINOS Y CONDICIONES GENERALES DE USO",
+      paragraph1: "El objeto de las condiciones: El Sitio Web",
+      paragraph2: "El objeto de las presentes Condiciones Generales de Uso (en adelante, Condiciones) es regular el acceso y la utilización del Sitio Web. A los efectos de las presentes Condiciones se entenderá como Sitio Web: la apariencia externa de los interfaces de pantalla, tanto de forma estática como de forma dinámica, es decir, el árbol de navegación; y todos los elementos integrados tanto en los interfaces de pantalla como en el árbol de navegación (en adelante, Contenidos) y todos aquellos servicios o recursos en línea que en su caso ofrezca a los Usuarios (en adelante, Servicios).",
+      paragraph3: "Let's cook se reserva la facultad de modificar, en cualquier momento, y sin aviso previo, la presentación y configuración del Sitio Web y de los Contenidos y Servicios que en él pudieran estar incorporados. El Usuario reconoce y acepta que en cualquier momento Let's cook pueda interrumpir, desactivar y/o cancelar cualquiera de estos elementos que se integran en el Sitio Web o el acceso a los mismos.",
+      paragraph4: "El acceso al Sitio Web por el Usuario tiene carácter libre y, por regla general, es gratuito sin que el Usuario tenga que proporcionar una contraprestación para poder disfrutar de ello, salvo en lo relativo al coste de conexión a través de la red de telecomunicaciones suministrada por el proveedor de acceso que hubiere contratado el Usuario.",
+      paragraph5: "La utilización de alguno de los Contenidos o Servicios del Sitio Web podrá hacerse mediante la suscripción o registro previo del Usuario."
+    },
+    thirdParagraph: {
+      subtitle: "El Usuario",
+      paragraph1: "El acceso, la navegación y uso del Sitio Web, así como por los espacios habilitados para interactuar entre los Usuarios, y el Usuario y Let's cook, como los comentarios y/o espacios de blogging, confiere la condición de Usuario, por lo que se aceptan, desde que se inicia la navegación por el Sitio Web, todas las Condiciones aquí establecidas, así como sus ulteriores modificaciones, sin perjuicio de la aplicación de la correspondiente normativa legal de obligado cumplimiento según el caso. Dada la relevancia de lo anterior, se recomienda al Usuario leerlas cada vez que visite el Sitio Web.",
+      paragraph2: "El Sitio Web de Let's cook proporciona gran diversidad de información, servicios y datos. El Usuario asume su responsabilidad para realizar un uso correcto del Sitio Web. Esta responsabilidad se extenderá a:",
+      paragraph3: "Un uso de la información, Contenidos y/o Servicios y datos ofrecidos por Let's cook sin que sea contrario a lo dispuesto por las presentes Condiciones, la Ley, la moral o el orden público, o que de cualquier otro modo puedan suponer lesión de los derechos de terceros o del mismo funcionamiento del Sitio Web. La veracidad y licitud de las informaciones aportadas por el Usuario en los formularios extendidos por Let's cook para el acceso a ciertos Contenidos o Servicios ofrecidos por el Sitio Web. En todo caso, el Usuario notificará de forma inmediata a Let's cook acerca de cualquier hecho que permita el uso indebido de la información registrada en dichos formularios, tales como, pero no sólo, el robo, extravío, o el acceso no autorizado a identificadores y/o contraseñas, con el fin de proceder a su inmediata cancelación. Let's cook se reserva el derecho de retirar todos aquellos comentarios y aportaciones que vulneren la ley, el respeto a la dignidad de la persona, que sean discriminatorios, xenófobos, racistas, pornográficos, spamming, que atenten contra la juventud o la infancia, el orden o la seguridad pública o que, a su juicio, no resultaran adecuados para su publicación.",
+    }
+  },
+  en: {
+    firstParagraph: {
+      subtitleI: "I. General information",
+      paragraph1: "En cumplimiento con el deber de información dispuesto en la Ley 34/2002 de Servicios de la SOciedad de la Información y el Comercio Electrónico (LSSI-CE) de 11 de julio, se facilitan a continuación los siguientes datos de información general de este sitio web:",
+      paragraph2: "La titularidad de este sitio web, www.letscooknow.es, (en adelante, Sitio Web) la ostenta: Let's Cook S.L., con CIF: B67476051, y cuyos datos de contacto son: ",
+      adress: "Address:",
+      email: "Contact email:",
+    }
+  }
+}
