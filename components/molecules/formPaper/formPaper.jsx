84504e5a00e06309eec49f1d775a3fa9c12953b2
Irungaray <LionelIrungaray@GMail.com>
2021-06-10 11:55:08 -0300

    Fix en FormPaper

--- components/molecules/formPaper/formPaper.jsx
+++ components/molecules/formPaper/formPaper.jsx
@@ -8,26 +8,17 @@ import PropTypes from "prop-types";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
-import Image from "next/image";
 import { Grid } from "@material-ui/core";
 
 const FormPaper = (props) => {
-    const { margin, paper, image, shadow, displayImage, title } = useStyles();
+    const { margin, paper, image, shadow, title } = useStyles();
 
     return (
-        <Box width={props.fullWidth ? "100%" : props.width} className={margin}>
-            <Grid item container direction="row" justify="center">
-                <Box className={displayImage}>
-                    <Image
-                        src="/assets/beneficios.jpg"
-                        width={500}
-                        height={620}
-                        // layout="fixed"
-                        className={clsx(image, shadow)}
-                    />
-                </Box>
+        // <Box width={props.fullWidth ? "100%" : props.width} className={margin}>
+            <Grid item container direction="row" justify="center" className={margin}>
+                <Box className={clsx(image, shadow)}></Box>
 
-                <Paper className={clsx(paper, shadow)}>
+                <Paper className={clsx(paper, shadow)} id="paper">
                     <Typography variant="h5" color="textSecondary" className={title}>
                         {props.title}
                     </Typography>
@@ -39,7 +30,7 @@ const FormPaper = (props) => {
                     {props.children}
                 </Paper>
             </Grid>
-        </Box>
+        // </Box>
     );
 };
 
