70e58e1835aa8374242162a7bb714339a8a8e05e
Irungaray <LionelIrungaray@GMail.com>
2021-06-15 12:00:53 -0300

    Integrado checkoutForms a BuyFlow

--- components/organisms/buyForm/checkoutStep.jsx
+++ components/organisms/buyForm/checkoutStep.jsx
@@ -1,14 +1,26 @@
-import { PropTypes } from "@material-ui/core";
+// Utils & Config
+import PropTypes from "prop-types";
 import { useBuyFlow } from "../../../stores/buyflow";
+
+// External components
+import { Container } from "@material-ui/core";
+
+// Internal components
+import ShipmentForm from '../../molecules/shipmentForm/shipmentForm';
+import PaymentForm from '../../molecules/paymentForm/paymentForm';
+import IconsWithText from '../../molecules/iconsWithText/iconsWithText';
 
 export const CheckoutStep = () => {
     const gotToNextView = useBuyFlow(({ forward }) => forward);
 
     return (
-        <>
-            CheckoutStep
+        <Container maxWidth="lg">
+            <ShipmentForm registeredUser />
+            <PaymentForm savedCards />
+            <IconsWithText />
+
             <button onClick={() => gotToNextView()}>Elegir recetas</button>
-        </>
+        </Container>
     );
 };
 
