ed42210a21fff5659c233b26fef1326a3b655889
Alejopdp <alejoscotti@gmail.com>
2021-06-30 16:04:35 +0200

    Phone number input added

---
+++ components/atoms/phoneNumberInput/phoneNumberInput.jsx
@@ -0,0 +1,33 @@
+//Utils & configs
+import React from "react";
+import PropTypes from "prop-types";
+import { useTheme } from "@material-ui/core";
+
+// External components
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/material.css";
+
+const PhoneNumberInput = (props) => {
+    const theme = useTheme();
+
+    return (
+        <>
+            <PhoneInput
+                country={"es"}
+                value={props.value}
+                inputStyle={{ backgroundColor: theme.palette.background.secondary, width: "100%" }}
+                dropdownStyle={{ backgroundColor: theme.palette.background.secondary, color: "gray", width: props.width }}
+                containerStyle={{ width: "100%" }}
+                specialLabel={""}
+                onChange={(newValue) => props.handleChange(newValue)}
+            />
+        </>
+    );
+};
+
+PhoneNumberInput.propTypes = {
+    value: PropTypes.string.isRequired,
+    handleChange: PropTypes.string.isRequired,
+};
+
+export default PhoneNumberInput;
