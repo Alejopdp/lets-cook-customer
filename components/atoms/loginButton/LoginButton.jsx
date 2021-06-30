ba96f7693a3b6955f34732e56da5e48fc3074e97
Alejopdp <alejoscotti@gmail.com>
2021-06-30 10:34:20 +0200

    Zustand added, logged in navbar added

--- components/atoms/loginButton/LoginButton.jsx
+++ components/atoms/loginButton/LoginButton.jsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { ButtonBase, makeStyles, Typography } from "@material-ui/core";
 import { PersonOutlineRounded } from "@material-ui/icons";
+// import { useRouter } from "next/router";
 
 const useStyles = makeStyles((theme) => ({
     loginButton: {
@@ -7,23 +9,26 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(1),
         borderRadius: 50,
         margin: `0 auto`,
-    }
+    },
 }));
 
-const LoginButton = ({ border=false, ...props }) => {
+const LoginButton = ({ border = false, ...props }) => {
     const classes = useStyles();
+    // const { push } = useRouter();
+
     return (
         <ButtonBase
             focusRipple
             className={classes.loginButton}
             style={{
-                border: border ? "solid 1px gray" : "none"
+                border: border ? "solid 1px gray" : "none",
             }}
+            // onClick={push("/ingresar")}
         >
             <PersonOutlineRounded style={{ marginRight: 4 }} />
             <Typography variant="button">Ingresar</Typography>
         </ButtonBase>
     );
-}
+};
 
 export default LoginButton;
