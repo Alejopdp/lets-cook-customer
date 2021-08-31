b70dfe35fe1d92c1c4904d7d80bd79b91579d58f
Alejopdp <alejoscotti@gmail.com>
2021-08-31 12:42:44 +0200

    refactor(cancellation): added cancellarion reason enum

---
+++ types/cancellation.ts
@@ -0,0 +1,10 @@
+export enum CancellationReason {
+    CREATED_BY_ERROR = "created_by_error",
+    CANT_GET_KITS_NEXT_WEEK = "cant_get_kits_next_week",
+    SPECIAL_DIET = "special_diet",
+    MOVE_ABROAD = "move_abroad",
+    DONT_LIKE_MEAL_KITS = "dont_like_meal_kits",
+    HAD_PROBLEMS_WITH_LETSCOOK = "had_problems_with_letscook",
+    PRICE_TOO_HIGH = "price_too_high",
+    OTHER_REASONS = "other_reason",
+}
