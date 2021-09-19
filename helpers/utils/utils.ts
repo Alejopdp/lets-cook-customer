549e9254167577215f6aff180fc502b6b87b7e41
Alejopdp <alejoscotti@gmail.com>
2021-09-20 01:06:23 +0200

    feat(utils): presenter for order number

--- helpers/utils/utils.ts
+++ helpers/utils/utils.ts
@@ -5,3 +5,7 @@ export const capitalizeFirstLetter = (text: string) => {
 export const roundTwoDecimals = (number: number) => {
     return Math.round((number + Number.EPSILON) * 100) / 100;
 };
+
+export const presentNumberWithHashtagAndDotSeparator = (number: number) => {
+    return `#${number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`;
+};
