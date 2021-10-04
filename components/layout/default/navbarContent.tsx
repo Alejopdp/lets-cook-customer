4d151be8751c1328d77d803eb081fb0c125df17c
Alejopdp <alejoscotti@gmail.com>
2021-10-04 18:20:38 +0200

    fix(logo): logo position fixed

--- components/layout/default/navbarContent.tsx
+++ components/layout/default/navbarContent.tsx
@@ -28,7 +28,7 @@ const useStyles = makeStyles((theme) => ({
         flex: 1,
         justifyContent: "center",
         [theme.breakpoints.up("sm")]: {
-            justifyContent: "right",
+            justifyContent: "flex-start",
         },
         cursor: "pointer",
     },
