72711f6014602085c89c7ffeab6697e5da14cdf1
Alejopdp <alejoscotti@gmail.com>
2021-12-13 08:00:51 -0300

    feat(404): custom 404 error page added

---
+++ lang/pages/404.jsx
@@ -0,0 +1,11 @@
+export const errorPage = {
+    es: {
+        notFoundError: "404 - Página no encontrada",
+    },
+    en: {
+        title: "404 - Page Not Found",
+    },
+    ca: {
+        title: "404 - Pàgina no trobada",
+    },
+};
