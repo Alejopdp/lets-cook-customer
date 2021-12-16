2c5a726603680ee723383653109b457b66b20cb7
Alejopdp <alejoscotti@gmail.com>
2021-12-16 17:45:32 -0300

    fix: rate recipe redirection

--- components/molecules/pendingActionsComponents/rateRecipesActionBox.jsx
+++ components/molecules/pendingActionsComponents/rateRecipesActionBox.jsx
@@ -7,13 +7,23 @@ import Typography from "@material-ui/core/Typography";
 
 // Internal components
 import BoxWithIconAndTextButton from "../../../components/molecules/specificBox/boxWithIconAndTextButton";
+import { useRouter } from "next/router";
+import { localeRoutes, Routes } from "lang/routes/routes";
+import { useUserInfoStore } from "@stores";
 
-
-const RateRecipesActionBox = ({lang}) => {
+const RateRecipesActionBox = ({ lang }) => {
     const theme = useTheme();
+    const router = useRouter();
+    const userInfo = useUserInfoStore((state) => state.userInfo);
 
     return (
-        <BoxWithIconAndTextButton icon="rating" btnText={lang.btnText}>
+        <BoxWithIconAndTextButton
+            icon="rating"
+            btnText={lang.btnText}
+            handleClick={() =>
+                router.push({ pathname: localeRoutes[router.locale][Routes["valorar-recetas"]], query: { customer: userInfo.id } })
+            }
+        >
             <Typography variant="body2" style={{ fontSize: "16px" }}>
                 {lang.text}
             </Typography>
