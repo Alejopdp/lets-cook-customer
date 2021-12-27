91d6ae555bb5828a6bdce73b32c050280c7245cf
Alejopdp <alejoscotti@gmail.com>
2021-12-27 23:41:30 +0100

    fix: sitemap added

---
+++ pages/api/my-sitemap.js
@@ -0,0 +1,18 @@
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { Readable } = require("stream");
+const { getPosts } = require("../../helpers/serverRequests/blog/index");
+
+export default generateSitemap = async (req, res) => {
+    const posts = await getPosts();
+
+    // An array with your links
+    const links = posts.map((post) => ({ url: post.slug, changefreq: "daily", priority: 0.3 }));
+
+    // Create a stream to write to
+    const stream = new SitemapStream({ hostname: `https://${process.env.NEXT_PUBLIC_DOMAIN}` });
+
+    // Return a promise that resolves with your XML string
+    const xmlString = await streamToPromise(Readable.from(links).pipe(stream)).then((data) => data.toString());
+
+    res.end(xmlString);
+};
