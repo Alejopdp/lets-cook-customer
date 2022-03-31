d9a6a25d37b28d52c082890db43a2cbd3b2b7f3f
Alejo Scotti <alejo@novolabs.xyz>
2022-04-01 00:01:33 +0200

    fix: showing error at sign up if email has an accent

--- helpers/utils/utils.ts
+++ helpers/utils/utils.ts
@@ -9,3 +9,17 @@ export const roundTwoDecimals = (number: number) => {
 export const presentNumberWithHashtagAndDotSeparator = (number: number) => {
     return `#${number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`;
 };
+
+export const hasAccents = (text: string): boolean => {
+    var accentArray = ["á", "à", "ã", "â", "é", "è", "ê", "í", "ì", "î", "õ", "ó", "ò", "ô", "ú", "ù", "û"];
+
+    for (var i = 0; i < text.length; i++) {
+        for (var j = 0; j < accentArray.length; j++) {
+            if (text[i] === accentArray[j]) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+};
--- lang/components/organisms/signupForm/mailStep.jsx
+++ lang/components/organisms/signupForm/mailStep.jsx
@@ -3,15 +3,18 @@ export const mailStep = {
         emailInput: "Tu correo electrónico",
         buttonText: "Continuar",
         emailAlreadyExistsError: "Ya existe un usuario con el correo ingresado",
+        accentError: "El correo electrónico no debe tener ningún acento",
     },
     en: {
         emailInput: "Your email",
         buttonText: "Next",
         emailAlreadyExistsError: "Another user already has the email",
+        accentError: "The email must not have any accent",
     },
     ca: {
         emailInput: "El teu correu electrònic",
         buttonText: "Continuar",
         emailAlreadyExistsError: "Ja existeix un usuari amb el correu ingressat",
+        accentError: "El correu electrònic no ha de tenir cap accent",
     },
 };
