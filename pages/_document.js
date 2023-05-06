ba76cb58688adaf1f12577fe2a35a75f7e9169be
Alejo Scotti <alejo@novolabs.xyz>
2023-05-06 18:38:59 +0200

    feat: gtm added

--- pages/_document.js
+++ pages/_document.js
@@ -42,11 +42,27 @@ export default class MyDocument extends Document {
                     `,
                         }}
                     />
+                    {/* <!-- Google Tag Manager --> */}
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                        (function(w,d,s,l,i) {w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-5M7F3L5');`
+                    }} />
+{/* <!-- End Google Tag Manager --> */}
+
                 </Head>
                 <body>
+                {/* <!-- Google Tag Manager (noscript) --> */}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5M7F3L5"
+height="0" width="0" style={{display: "none", visibility:"hidden"}}></iframe></noscript>
+{/* <!-- End Google Tag Manager (noscript) → */}
                     <Main />
                     <NextScript />
-                    <script async type="text/javascript" src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=YqjdW7"></script>
+                    <script async type="text/javascript" src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=YqjdW7" /> 
                 </body>
             </Html>
         );
